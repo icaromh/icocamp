f937bdc53a7a4ed725d493999448c7f17edbd76b
Marcie <gottschalkmarciele@gmail.com>
2026-01-22 12:06:31 +0100

    solução desafio 4 - curso - primeiros métodos adicionados e funcionando

---
+++ challenges/04-curso.js
@@ -0,0 +1,52 @@
+/*You are building the logic for a learning platform. You need an object that stores course 
+topics, tracks completion, and calculates the overall progress percentage dynamically. 
+To keep your code clean, you will use a Factory Function to generate your topic objects.
+ */
+
+/* PART 1: FACTORY FUNCTIONS
+Create a function topicFactory(title, difficulty) that returns an object with: 
+    - title: The name of the topic.
+    - difficulty: The level (Beginner, Intermediate, or Advanced).
+    - isCompleted: Set to false by default.
+*/
+
+function topicFactory(title, difficulty) {
+    return {
+        title: title,
+        difficulty: difficulty,
+        isCompleted: false
+    };
+}
+
+const myBootcamp = {
+    name: 'PrincessCamp',
+    topics: [], //An array to hold the objects created by your factory.
+    //METHODS
+    addTopic: function (tittle, difficulty) {
+        this.topics.push(topicFactory(tittle, difficulty));
+    },
+    listAll: function () {
+        this.topics.forEach(element => {
+            console.log(`[ ] ${element['title']} (${element['difficulty']});`)
+            //console.log(`[ ] ${this.topics[element][title]} (${this.topics[element][difficulty]});`)
+        });
+    },
+    markAsComplete: function () {
+
+    }
+};
+
+//adding the data :)
+myBootcamp.addTopic('Welcome to Learn JavaScript', 'Begginer');
+myBootcamp.addTopic('Introduction', 'Begginer');
+myBootcamp.addTopic('Conditionals', 'Intermediate');
+myBootcamp.addTopic('Functions', 'Intermediate');
+myBootcamp.addTopic('Scope', 'Advanced');
+myBootcamp.addTopic('Arrays ', 'Intermediate');
+myBootcamp.addTopic('Loops ', 'Intermediate');
+myBootcamp.addTopic('Iterators', 'Advanced');
+myBootcamp.addTopic('Objects', 'Advanced');
+myBootcamp.addTopic('Next steps', 'Begginer');
+
+//listando o curso completo
+myBootcamp.listAll();
